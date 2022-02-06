8f88b04a77b9e431eb1936f82d9525391555d848
imsahiljain <emailofsahiljain@gmail.com>
2022-02-06 19:21:39 +0530

    change spos syntax

--- components/age-content/child.jsx
+++ components/age-content/child.jsx
@@ -32,16 +32,17 @@ const Child = () => {
           <ModalHeader>Childhood</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            It's never too early to talk with your child about sex. Talking
+            It&apos;s never too early to talk with your child about sex. Talking
             about sex, sexuality and bodies as your child moves towards puberty
             can help your child understand that sex and sexuality are typical,
             healthy parts of life. Open and honest conversations when your child
             is young can make later conversations easier. And these early
             conversations also lay the groundwork for children to make healthier
-            and safer choices about sex and sexual relationships when they're
-            older. The key early message for your child is that he can come to
-            you for open, honest and reliable information, and that he shouldn't
-            feel scared or embarrassed to ask you about sex and sexuality.
+            and safer choices about sex and sexual relationships when
+            they&apos;re older. The key early message for your child is that he
+            can come to you for open, honest and reliable information, and that
+            he shouldn&apos;t feel scared or embarrassed to ask you about sex
+            and sexuality.
           </ModalBody>
 
           <ModalFooter>
